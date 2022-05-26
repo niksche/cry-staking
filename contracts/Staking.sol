0ce729c7a9b60d2ab07fd51b4b632714bc0c167b
Nikita Shchelkanov <nik.schelkanov@gmail.com>
2022-05-26 15:16:28 +0300

    [ADD] Added core of staking

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -7,40 +7,57 @@ import "./ERC20.sol";
 
 
 contract Staking {
-    mapping(address => uint) public balances;
-    uint public StakingsRevardPercent;
-    uint public StakingsTime;
 
-    ERC20 public cryToken;
-    ERC20 public lpToken;
+    mapping(address => uint) public lpTokensbalances;
+    mapping(address => uint) public stakingTimestamp;
+    mapping(address => bool) public claimedRevard;
 
-    constructor(address _lpTokenAddress,address _cryTokenAddress) {
-        cryToken = ERC20(_lpTokenAddress);
-        lpToken = ERC20(_cryTokenAddress);
-        console.log("done");
+    uint public totalStakedAmount;
+    uint public stakingsRevardPercent;
+    uint public stakingTime;
+
+    ERC20 public rewardToken; // - CRY token
+    ERC20 public lpToken; // - Any other token you would love to chose;
+
+    constructor(address _lpTokenAddress, address _rewardTokenAddress) {
+        rewardToken = ERC20(_lpTokenAddress);
+        lpToken = ERC20(_rewardTokenAddress);
+        stakingTime = 0 minutes;
     }
 
-    // withdraw from msg.sender amount of token
+    // accept lp token from msg.sender => after 10 minutes it will 
     function stake(uint amount) public {
+        require(block.timestamp >= stakingTimestamp[msg.sender] + stakingTime, "Previous staking is active");
         // will work only if approved previoslly was called
-        cryToken.transferFrom(msg.sender, address(this), amount);
-        balances[msg.sender] += amount;
+        lpToken.transferFrom(msg.sender, address(this), amount);
+        lpTokensbalances[msg.sender] += amount;
+        totalStakedAmount += amount;
+        stakingTimestamp[msg.sender] = block.timestamp;
+        claimedRevard[msg.sender] = false;
     }
 
-    // function claim() public {
+    function claim() public {
+        require(claimedRevard[msg.sender] == false, "Easy cowboy");
+        require(lpTokensbalances[msg.sender] != 0, "User must have at least something to be deposited");
+        require(block.timestamp >= stakingTimestamp[msg.sender] + 2 * stakingTime, "Too early for withdrawing money honey");
 
-    // }
+        uint rewardTokenAmount = stakingsRevardPercent * lpTokensbalances[msg.sender] / 100; 
+        rewardToken.transfer(msg.sender, rewardTokenAmount);
+        claimedRevard[msg.sender] = true;
 
-    // function unstake() public {
+    }
 
-    // }
+    function unstake() public {
+        require(lpTokensbalances[msg.sender] != 0, "User must have at least something to be deposited");
+        require(block.timestamp >= stakingTimestamp[msg.sender] + 2 * stakingTime, "Too early for withdrawing money honey");
+        lpToken.transfer(msg.sender, lpTokensbalances[msg.sender]);
+    }
 
-    // function changeStakingsTime(uint newTime) private {
-    //     StakingsTime = newTime;
-    // }
+    function changeStakingsTime(uint _stakingTime) private {
+        stakingTime = _stakingTime;
+    }
 
-    // function changeStakingsRevardPercent(uint newPercent) private {
-    //     StakingsRevardPercent = newPercent;
-    // }
-
+    function changeStakingsRevardPercent(uint _stakingsRevardPercent) private {
+        stakingsRevardPercent = _stakingsRevardPercent;
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,26 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Staking", function () {
+  let owner: SignerWithAddress;
+  let address2: SignerWithAddress;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function() {
+    
+    [owner, address2] = await ethers.getSigners();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let stakingContractFactory = await ethers.getContractFactory("Staking", owner);
+    stakingContractFactory.deploy("todo: someaddress", "someaddress");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});``
+  })
+
+  describe("Deployment", function() {
+    it("Should deploy contract and set something to smth", function() {
+      expect(BigNumber.from("0")).to.be.equal(BigNumber.from("0"));
+    })
+  })
+})
+
